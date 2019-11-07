a293fc10db3938ac88d62274c83a0587d49868a1
Rifki Athar <athar.rosyadi@gmail.com>
2019-11-07 21:03:01 +0700

    master of promah project

---
+++ app/src/main/java/com/arosyadi/promah/MainActivity.java
@@ -0,0 +1,20 @@
+package com.arosyadi.promah;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/arosyadi/promah/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.arosyadi.promah.base;
+
+import androidx.fragment.app.Fragment;
+
+public class BaseFragment extends Fragment {
+
+    public static final String KEY_SCHEDULED = "scheduled";
+    public static final String KEY_ONGOING = "ongoing";
+    public static final String KEY_COMPLETED = "completed";
+}
